a24d288bb39e222fba0165b404d0d21f1bcc4685
Carlos Estay Olguín <cfestay@gmail.com>
2021-05-13 11:57:08 -0400

    Commit inicial

---
+++ Challenge_Java_Alkemy-3/src/main/java/cl/carlos/alkemy/ChallengeJavaAlkemy3Application.java
@@ -0,0 +1,13 @@
+package cl.carlos.alkemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeJavaAlkemy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeJavaAlkemy3Application.class, args);
+	}
+
+}
---
+++ Challenge_Java_Alkemy-3/src/test/java/cl/carlos/alkemy/ChallengeJavaAlkemy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.carlos.alkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeJavaAlkemy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
