7ff092c63a2bc91e2b72a04e7880081742368be9
Carlos Estay Olguín <cfestay@gmail.com>
2021-05-16 07:02:07 -0400

    Entities created. Close #3

---
+++ Challenge_Java_Alkemy-3/src/main/java/cl/carlos/alkemy/controller/PersonController.java
@@ -0,0 +1,16 @@
+package cl.carlos.alkemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class PersonController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "personajes";
+	}
+
+}
